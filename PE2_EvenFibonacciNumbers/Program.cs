b2a9e09e9c6e7d7f61960bebe9bf7a889952e856
mehmetkillar <49925113+mehmetkillar@users.noreply.github.com>
2021-12-20 14:26:39 +0300

    Add project files.

---
+++ PE2_EvenFibonacciNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PE2_EvenFibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                long n = Convert.ToInt64(Console.ReadLine());
+                long fb1 = 1;
+                long fb2 = 1;
+                long fbSwap;
+                long sum = 0;
+                while (n > fb2)
+                {
+                    fbSwap = fb2;
+                    fb2 += fb1;
+                    fb1 = fbSwap;
+                    if (fb2 % 2 == 0 && n > fb2) sum += fb2;
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
---
+++ PE3_LargestPrimeFactor/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace PE3_LargestPrimeFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                long n = Convert.ToInt64(Console.ReadLine());
+                //if(n%2==0)
+                //for(int i = 0; i < n/2; i+=2)
+                //{
+
+                //}
+                //for (long i = 2; n%i==0 && n>i; i++)
+                //{
+                //    n /= i;
+                //    Console.WriteLine(i);
+                //}
+
+
+                //for (i = 3; i < n; i+=2)
+                //{
+
+                //}
+                while (n%2==0)n /= 2;
+                if (n < 2) Console.WriteLine(2);
+                else
+                {
+                    int i = 3;
+                    while (n > i)
+                    {
+                        if (n % i == 0) n /= i;
+                        else if (n > i) i += 2;
+                    }
+                    Console.WriteLine(i);   
+                }
+                //while (i < n)
+                //{
+                //    while (n % i == 0)
+                //    {
+                //        n /= i;
+                //    }
+                //    if(n>i) i += 2;
+                //}
+                //Console.WriteLine(i);
+
+
+                //switch (n)
+                //{
+                //    case 2:
+                //        i = 2;
+                //        Console.WriteLine(i);
+                //        break;
+                //    default:
+                //        while (n > 1)
+                //        {
+                //            if (n % i == 0)
+                //            {
+                //                n /= i;
+                //                Console.WriteLine(i);
+                //            }
+                //            else i += 2;
+                //        }
+                //        break;
+                //}
+
+
+            }
+
+        }
+
+        //static int mm(long n)
+        //{
+        //    while (n > 1)
+        //    {
+        //        if (n % i == 0)
+        //        {
+        //            n /= i;
+        //            Console.WriteLine(i);
+        //        }
+        //        else i += 2;
+        //    }
+        //}
+    }
+}
